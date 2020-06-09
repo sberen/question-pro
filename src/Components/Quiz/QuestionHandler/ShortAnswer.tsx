1eab764277f348380980f39af3c3497951618df0
Zizhen Song <zizhensong@gmail.com>
2020-06-09 09:46:42 -0700

    Created generic Question handler to be extended

--- src/Components/Quiz/QuestionHandler/ShortAnswer.tsx
+++ src/Components/Quiz/QuestionHandler/ShortAnswer.tsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import { Button, TextField } from '@material-ui/core';
+import { QuestionHandler } from './QuestionHandler';
 
-interface QuestionProps {
-    question: any,
-    changeAnswer(ans: string): void,
-    changeQuestion(num: number): void,
-    isFirst: boolean,
-    isLast: boolean,
-    answer: string
-}
-
-
-export class ShortAnswer extends React.Component<QuestionProps, {}> {
-    
-    onInputChange = (event: any) => {
-        this.props.changeAnswer(event.target.value);
-    }
+export class ShortAnswer extends QuestionHandler {
 
     render() {
-        const result : any[] = []
-        var secondButton:String;
-        if(this.props.isLast){
-            secondButton = "Finish"
-        } else{
-            secondButton = "Next"
-        }
-        if (!this.props.isFirst){
-            result.push(
-                <Button key='back' onClick={() => this.props.changeQuestion(-1)} variant='outlined' color='primary'>Back</Button>
-            )
-        }
-        result.push(
-            <Button key="next" onClick={() => {this.props.changeQuestion(1)}} variant='outlined' color='primary'>
-            {secondButton}</Button>
-        )
+        let result = this.renderButtons();
         
-
         return (
         <div>
             {this.props.question.prompt} <br/>
