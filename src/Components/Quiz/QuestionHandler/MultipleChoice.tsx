fa044b6e4c0b2b9ab65e56ff3c1bd81b0f2e000b
Zizhen Song <zizhensong@gmail.com>
2020-06-09 10:51:47 -0700

    Multiple Choice question format implemented

---
+++ src/Components/Quiz/QuestionHandler/MultipleChoice.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+import { QuestionHandler } from './QuestionHandler';
+
+export class MultipleChoice extends QuestionHandler {
+
+    renderChoices = () => {
+
+        let options = this.props.question.choices.map((item: string) =>
+            <FormControlLabel value={item} control={<Radio />} label={item} />
+        );
+        return (
+            <FormControl component="fieldset">
+                <FormLabel component="legend">{this.props.question.prompt}</FormLabel>
+                <RadioGroup aria-label="question" name="question1" value={this.props.answer} onChange={this.onInputChange}>
+                    {options}
+                </RadioGroup>
+            </FormControl>
+        );
+    }
+    render() {
+        let result = this.renderButtons();
+        
+        return (
+        <div>
+            {this.renderChoices()}
+            {result}
+        </div>);
+    }
+}
