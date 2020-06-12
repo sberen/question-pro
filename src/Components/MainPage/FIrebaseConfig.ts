67a7ab78ecf34549475ea2f76d73043cfa1508b3
Zizhen Song <zizhensong@gmail.com>
2020-06-12 12:52:33 -0700

    Added public key

---
+++ src/Components/MainPage/FIrebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FIREBASECONFIG = {
+    apiKey: "AIzaSyBVtbyerfW6d_477bvl1ffMj1Wez75cakg",
+    authDomain: "questionpro-65223.firebaseapp.com",
+    databaseURL: "https://questionpro-65223.firebaseio.com",
+    projectId: "questionpro-65223",
+    storageBucket: "questionpro-65223.appspot.com",
+    messagingSenderId: "880645784876",
+    appId: "1:880645784876:web:c0aee748808ff73ab65889",
+    measurementId: "G-S6W8V13Y0E"
+};
