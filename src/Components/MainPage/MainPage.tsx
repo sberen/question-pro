0d59162e4126ac8329a0bebab5d8158323ed2e91
Zizhen Song <zizhensong@gmail.com>
2020-06-12 12:32:12 -0700

    Established API config file

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -7,6 +7,7 @@ import {QuizHandler} from '../Quiz/QuizHandler'
 import UploadQuiz from '../UploadQuiz/UploadQuiz';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import { FIREBASECONFIG } from './FIrebaseConfig';
 
 
 
@@ -37,18 +38,8 @@ export class MainPage extends React.Component<{}, MainPageState> {
 
   componentDidMount() {
 
-    const firebaseConfig = {
-      apiKey: "AIzaSyBVtbyerfW6d_477bvl1ffMj1Wez75cakg",
-      authDomain: "questionpro-65223.firebaseapp.com",
-      databaseURL: "https://questionpro-65223.firebaseio.com",
-      projectId: "questionpro-65223",
-      storageBucket: "questionpro-65223.appspot.com",
-      messagingSenderId: "880645784876",
-      appId: "1:880645784876:web:c0aee748808ff73ab65889",
-      measurementId: "G-S6W8V13Y0E"
-    };
 
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(FIREBASECONFIG);
 
     var db = firebase.firestore();
 
