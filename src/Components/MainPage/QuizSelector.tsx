bcab55502191381a0f66da665a360de0b8cbf4c9
Zizhen Song <zizhensong@gmail.com>
2020-06-22 10:12:28 -0700

    Implimented delete quiz

--- src/Components/MainPage/QuizSelector.tsx
+++ src/Components/MainPage/QuizSelector.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Button, Grid, Card, CardContent, Typography, CardActions} from '@material-ui/core';
+import {Button, Grid, Card, CardContent, Typography, CardActions, IconButton} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './QuizSelector.css';
 import { QuizInfo } from '../Quiz/QuizInfo';
 import { QUIZ_INDICES, QUIZ_TYPES } from '../Quiz/QuizTypes';
 import { QuizInfoMini } from '../Quiz/QuizInfoMini';
-import { firestore } from '../../firebase';
+import { firestore, auth } from '../../firebase';
+import firebase from 'firebase/app';
 
 interface SelectorProps {
   changeQuiz(qz : QuizInfo) : void;
   makeQuiz: () => void;
+  removeQuiz(id : string) : void;
   quizzes: QuizInfoMini[];
 }
 
@@ -24,6 +27,9 @@ export class QuizSelector extends React.Component<SelectorProps, {}> {
           </CardContent>
           <CardActions>
             <Button key={idx} onClick={() => this.selectQuiz(quiz.uid)} color='primary' variant='text'>Take Quiz</Button>
+            <IconButton aria-label="delete" style={{ color: "#e32636"}} onClick={() => this.removeQuiz(quiz.uid)}>
+              <DeleteIcon />
+            </IconButton>
           </CardActions>
         </Grid>
       ));
@@ -50,5 +56,14 @@ export class QuizSelector extends React.Component<SelectorProps, {}> {
     let object = await firestore.collection("quizzes").doc(quiz).get();
     let qz = new QuizInfo(object.get("title"), object.get("type"), quiz, object.get("questions"));
     this.props.changeQuiz(qz);
+  }
+
+  removeQuiz(quiz : string){
+    let object = firestore.collection("users").doc(auth.currentUser!.uid);
+    var key = `quizzes.${quiz}`;
+    object.update({
+      [key]: firebase.firestore.FieldValue.delete()
+    });
+    this.props.removeQuiz(quiz);
   }
 }
